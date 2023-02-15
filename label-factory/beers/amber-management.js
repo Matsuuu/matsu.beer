c02f58964919ef2af62121176a0eb52179e1bcfd
Matsuuu <huhta.matias@gmail.com>
2023-02-15 14:34:41 +0200

    Update some labels

--- label-factory/beers/amber-management.js
+++ label-factory/beers/amber-management.js
@@ -5,15 +5,13 @@ export const AmberManagement = html`
     beer-name="Amber Management"
     beer-style="Amber Ale"
     abv="4.2%"
-    contains="Malts and Hops"
+    contains="Malts, hops and yeast"
     theme-color="255,191,0"
     style="--title-size: 3.6cqw"
   >
+    <img slot="description" style="filter: invert(1); margin: 0 0 0rem 0" src="https://simplr.fi/assets/simplr_horisontal_black.svg" />
     <p slot="description">
-      Temper is the one thing you can't get rid of, by losing it.
-    </p>
-    <p slot="description">
-      For that reason, sit down, relax and grab yourself one a bottle of Amber
+      Sit down, relax and grab yourself one a bottle of Amber
       Management. The smooth caramelly flavor will sooth you down in an instant.
     </p>
   </beer-label>
