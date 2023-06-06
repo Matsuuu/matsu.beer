84e81ab7a126b6a83c6de53678fb66e2da932fda
matias.huhta@fcg.fi <Matias.Huhta@fcg.fi>
2023-06-06 12:46:06 +0300

    Juice shop

--- label-factory/beers/juice-shop.js
+++ label-factory/beers/juice-shop.js
@@ -6,12 +6,11 @@ export const JuiceShop = html`
     beer-style="Fruited Witbier"
     abv="5.0%"
     contains="Banana, coriander and orange peel"
-    theme-color="255,191,0"
+    theme-color="255,225,53"
     style="--title-size: 3.6cqw"
   >
     <p slot="description">
-      Sit down, relax and grab yourself one a bottle of Amber
-      Management. The smooth caramelly flavor will sooth you down in an instant.
+        We heard you like banana in your witbier so we put banana onto your brew to boost the banana from the banana filled yeast esters.
     </p>
   </beer-label>
 `;
